a6d6008438bb4c8c824168804885307081e3da4f
Cleyson <cleysonph@gmail.com>
2024-02-22 22:25:02 -0300

    04:08 - Criando a rota de login

--- src/main/java/br/com/treinaweb/twjobs/api/auth/controllers/AuthRestController.java
+++ src/main/java/br/com/treinaweb/twjobs/api/auth/controllers/AuthRestController.java
@@ -1,6 +1,8 @@
 package br.com.treinaweb.twjobs.api.auth.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.treinaweb.twjobs.api.auth.dtos.LoginRequest;
+import br.com.treinaweb.twjobs.api.auth.dtos.TokenResponse;
 import br.com.treinaweb.twjobs.api.auth.dtos.UserRequest;
 import br.com.treinaweb.twjobs.api.auth.dtos.UserResponse;
 import br.com.treinaweb.twjobs.api.auth.mappers.UserMapper;
 import br.com.treinaweb.twjobs.core.repositories.UserRepository;
+import br.com.treinaweb.twjobs.core.services.jwt.JwtService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -21,8 +26,10 @@ import lombok.RequiredArgsConstructor;
 public class AuthRestController {
 
     private final UserMapper userMapper;
+    private final JwtService jwtService;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     @ResponseStatus(code = HttpStatus.CREATED)
@@ -32,6 +39,18 @@ public class AuthRestController {
         user.setPassword(passwordHash);
         user = userRepository.save(user);
         return userMapper.toUserResponse(user);
+    }
+
+    @PostMapping("/login")
+    public TokenResponse login(@RequestBody @Valid LoginRequest loginRequest) {
+        var usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+            loginRequest.getEmail(),
+            loginRequest.getPassword()
+        );
+        authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        return TokenResponse.builder()
+            .accessToken(jwtService.generateAccessToken(loginRequest.getEmail()))
+            .build();
     }
     
 }
--- src/main/java/br/com/treinaweb/twjobs/config/SecurityConfig.java
+++ src/main/java/br/com/treinaweb/twjobs/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package br.com.treinaweb.twjobs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +24,13 @@ public class SecurityConfig {
                 .disable()
             )
             .build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
     
 }
