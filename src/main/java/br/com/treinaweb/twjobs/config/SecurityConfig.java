21780f7cfaeee3beafd4ee00c949a9cfec78df7c
Cleyson <cleysonph@gmail.com>
2024-02-22 23:02:53 -0300

    04:09 - Criando o filtro de autenticação via JWT

--- src/main/java/br/com/treinaweb/twjobs/config/SecurityConfig.java
+++ src/main/java/br/com/treinaweb/twjobs/config/SecurityConfig.java
@@ -7,12 +7,20 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.treinaweb.twjobs.api.common.filters.AccessTokenRequestFilter;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    private final AccessTokenRequestFilter accessTokenRequestFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,6 +31,10 @@ public class SecurityConfig {
             .csrf(customizer -> customizer
                 .disable()
             )
+            .sessionManagement(customizer -> customizer
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .addFilterBefore(accessTokenRequestFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
